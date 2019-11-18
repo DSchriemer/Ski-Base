18ce816037f92cff7e8029875e42af165f089983
David S <david.schriemer7@gmail.com>
2019-11-17 22:20:04 -0700

    Finally parsing data

--- app/src/main/java/com/example/skibase/MainActivity.java
+++ app/src/main/java/com/example/skibase/MainActivity.java
@@ -8,25 +8,79 @@ import android.widget.Button;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-    Button buttonA;
-    TextView txt;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button getBtn;
+    TextView result;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonA = (Button)findViewById(R.id.buttonA);
-        txt = (TextView)findViewById(R.id.txt);
-        buttonA.setOnClickListener(this);
+        result = (TextView) findViewById(R.id.result);
+        getBtn = (Button) findViewById(R.id.launchBtn);
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWebsite();
+            }
+        });
+
     }
 
-    @Override
-    public void onClick(View view) {
-        if(view.getId()==R.id.buttonA){
-            txt.setText("button clicked");
-        }
+   // @Override
+    //public void onClick(View view) {
+    //    if(view.getId()==R.id.buttonA){
+    //        txt.setText("button clicked");
+    //    }
+    //}
+
+    private void getWebsite()
+    {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+
+                try {
+                    System.out.println("Trying here!!!");
+                    Document doc = Jsoup.connect("http://www.ssaurel.com/blog").get();
+                    System.out.println("Trying here1.5!!!");
+                    String title = doc.title();
+                    Elements links = doc.select("a[href]");
+                    System.out.println("Trying here2!!!");
+                    builder.append(title).append("\n");
+                    System.out.println("Trying here3!!!");
+                    for ( Element link : links){
+                        System.out.println("ow ow ow!!!");
+                        builder.append("\n").append("Link: ").append(link.attr("href"))
+                                .append("\n").append("Text: ").append(link.text());
+                    }
+                }
+                catch (IOException e) {
+                    builder.append("error").append(e.getMessage()).append("\n");
+                    System.out.println(e.getMessage());
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.setText(builder.toString());
+                    }
+                });
+
+
+            }
+        }).start();
+
     }
 }
