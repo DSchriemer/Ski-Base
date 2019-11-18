957cc7ea13201b3fd6198c7265b0b40ac0c5d65d
David S <david.schriemer7@gmail.com>
2019-11-17 17:51:22 -0700

    Finishing base files

--- app/src/main/java/com/example/skibase/MainActivity.java
+++ app/src/main/java/com/example/skibase/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.skibase;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button buttonA;
     TextView txt;
@@ -18,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         buttonA = (Button)findViewById(R.id.buttonA);
         txt = (TextView)findViewById(R.id.txt);
+        buttonA.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.buttonA){
+            txt.setText("button clicked");
+        }
     }
 }
