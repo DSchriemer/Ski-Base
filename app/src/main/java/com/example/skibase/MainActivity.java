d7d7532b01d9a7fa21ecded59e9ef3bded6b6e08
David S <david.schriemer7@gmail.com>
2019-12-06 22:35:40 -0700

    Parsing highs is working!

--- app/src/main/java/com/example/skibase/MainActivity.java
+++ app/src/main/java/com/example/skibase/MainActivity.java
@@ -138,13 +138,13 @@ public class MainActivity extends AppCompatActivity {
 
                         //tmp.setText("y");
 
-                        for(int i = 0; i<40;i++)
+                        for(int i = 0; i<70;i++)
                         {
                             System.out.println("HERE: " + i + " " + (temp.charAt(i)));
 
                             if (tvcounter <= 2)
                             {
-                                if((String.valueOf(temp.charAt(i))).matches(".*[0-9].*") || (String.valueOf(temp.charAt(i)) == "-"))
+                                if((String.valueOf(temp.charAt(i))).matches(".*[0-9].*") || (String.valueOf(temp.charAt(i)).matches("-")))
                                 {
                                     System.out.println("Good: ");// + String.valueOf(temp.charAt(i)));
                                     textViewArray[tvcounter].append(String.valueOf(temp.charAt(i)));
