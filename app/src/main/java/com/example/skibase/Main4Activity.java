e34d02491d1202c5175af0b9368745f6a392ea5e
David S <david.schriemer7@gmail.com>
2020-04-26 18:36:36 -0700

    Added a sqlite database to the app and added the log book section to track ski days

--- app/src/main/java/com/example/skibase/Main4Activity.java
+++ app/src/main/java/com/example/skibase/Main4Activity.java
@@ -1,38 +1,63 @@
 package com.example.skibase;
-
-import androidx.annotation.NonNull;
+import android.content.Intent;
+import android.database.sqlite.SQLiteOpenHelper;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Main4Activity extends AppCompatActivity {
 
-    EditText txtname, txtage;
-    DatabaseReference reff;
-    Button btnsave;
-    Member member;
-
-    long maxid = 0;
+    DatabaseHelper myDB;
+    Button btnAdd,btnView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        editText = (EditText) findViewById(R.id.editText);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnView = (Button) findViewById(R.id.btnView);
+        myDB = new DatabaseHelper(this);
 
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = editText.getText().toString();
+                if(editText.length()!= 0){
+                    AddData(newEntry);
+                    editText.setText("");
+                }else{
+                    Toast.makeText(Main4Activity.this, "You must put something in the text field!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
-
-        }
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main4Activity.this, ViewListContents.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
 
+    public void AddData(String newEntry) {
+
+        boolean insertData = myDB.addData(newEntry);
+
+        if(insertData==true){
+            Toast.makeText(this, "Data Successfully Inserted!", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this, "Something went wrong :(.", Toast.LENGTH_LONG).show();
+        }
+    }
+}
